9bb6ae069d44849739208e04a83ffe4c5dbdd1f9
Felix <felixsteen@gmail.com>
2023-12-05 12:11:32 +0100

    changes services & models

--- adressbok/Services/CustomerAdd.cs
+++ adressbok/Services/CustomerAdd.cs
@@ -1,11 +1,22 @@
 ﻿
 
 using adressbok.Models;
+using System.Reflection.Metadata;
 
 namespace adressbok.Services
 {
     internal class CustomerAdd
     {
-        private CustomerModel;
+        public void Run()
+        {
+            var customer = new CustomerModel();
+
+            customer.FirstName = "Felix";
+            customer.LastName = "Sten";
+            customer.PhoneNumber = "1234567890";
+            customer.Email = "test@mail.com";
+            customer.Address = "test";
+        }
+        
     }
 }
