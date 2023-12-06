b6f966bfe08e80a156a7d307783587c933b94df4
Felix <felixsteen@gmail.com>
2023-12-06 10:44:20 +0100

    ViewSearchMenu & ViewDeleteMenu

--- adressbok/Services/MenuService.cs
+++ adressbok/Services/MenuService.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using adressbok.Models;
+using System;
 using System.Reflection.Metadata;
 
 namespace adressbok.Services
@@ -16,18 +17,28 @@ namespace adressbok.Services
                 Console.Clear();
                 Console.WriteLine("---MENU---");
                 Console.WriteLine();
-                Console.WriteLine("1. Register a customer");
-                Console.WriteLine("2. View all customers");
+                Console.WriteLine("1. Register a person");
+                Console.WriteLine("2. View all registered");
+                Console.WriteLine("3. Search for specific person");
+                Console.WriteLine("4. Delete a registered person");
 
                 var option = Console.ReadLine();
 
                 switch(option)
                 {
                     case "1":
-                        RegisterMenu();
+                        ViewRegisterMenu();
                         break;
                     case "2":
                         ViewAllMenu();
+                        break;
+                    case "3":
+                        ViewSearchMenu();
+                        break;
+                    case "4":
+                        ViewDeleteMenu();
+                        break;
+                    default: Console.WriteLine("Invalid option. Choose 1 2 3 or 4.");
                         break;
 
                 }
@@ -38,7 +49,7 @@ namespace adressbok.Services
         }
     
 
-        private void RegisterMenu()
+        private void ViewRegisterMenu()
         {
         var customer = new CustomerModel();
 
@@ -68,15 +79,76 @@ namespace adressbok.Services
         {
             var customers = _customerService.GetCustomersFromList();
 
+            Console.Clear();
+
             foreach (var customer in customers)
             {
-                Console.WriteLine($"{customer.FirstName} {customer.LastName} {customer.PhoneNumber} {customer.Email} {customer.Address}");
+                Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+                Console.WriteLine($"Phone number: {customer.PhoneNumber}");
+                Console.WriteLine($"Email address: {customer.Email}");
+                Console.WriteLine($"Home address: {customer.Address}");
+                Console.WriteLine("-----------------------------------");
             }
 
 
         }
 
 
+        private void ViewSearchMenu()
+        {
+            var customers = _customerService.GetCustomersFromList();
+            
+
+
+            Console.Clear();
+            Console.Write("Enter email address to search for person: ");
+            string searchEmail = Console.ReadLine()!;
+
+            CustomerModel customer = customers.Find(p => p.Email.Equals(searchEmail, StringComparison.OrdinalIgnoreCase))!;
+
+            if (customer != null)
+            {
+                Console.WriteLine($"Person found: {customer.FirstName} {customer.LastName}");
+                Console.WriteLine($"Phone number: {customer.PhoneNumber}");
+                Console.WriteLine($"Email address: {customer.Email}");
+                Console.WriteLine($"Home address: {customer.Address}");
+            }
+            else
+            {
+                Console.WriteLine("Could not find any person with this email in the list.");
+            }
+
+        }
+
+        private void ViewDeleteMenu()
+        {
+            var customers = _customerService.GetCustomersFromList();
+
+
+
+            Console.Clear();
+            Console.Write("Enter email address to delete person from the list: ");
+            string searchEmail = Console.ReadLine()!;
+
+            CustomerModel customer = customers.Find(p => p.Email.Equals(searchEmail, StringComparison.OrdinalIgnoreCase))!;
+
+            if (customer != null)
+            {
+                Console.WriteLine($"Person found: {customer.FirstName} {customer.LastName}");
+
+                customers.Remove(customer);
+                Console.WriteLine("Person has been deleted from the list.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Could not find any person with this email to delete from the list.");
+            }
+
+        }
+
+
     }
+
 
 }
